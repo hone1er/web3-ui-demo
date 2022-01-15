c50ccf35d405e85d07a0f28504b53bcfb17d2d8f
hone1er <24376928+hone1er@users.noreply.github.com>
2022-01-14 20:05:35 -0800

    added back and next button to basics page

--- pages/basics.js
+++ pages/basics.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "tailwindcss/tailwind.css";
+import Link from "next/link";
 import UsingConnectWallet from "../components/UsingConnectWallet";
 import UsingAddress from "../components/UsingAddress";
 const Basics = () => {
@@ -7,6 +8,18 @@ const Basics = () => {
     <div>
       <UsingConnectWallet />
       <UsingAddress />
+      <div className="flex w-full justify-center items-center">
+        <Link href="/">
+          <p className="py-4 px-8 m-4 hover:cursor-pointer bg-slate-600 rounded-lg text-slate-100">
+            back
+          </p>
+        </Link>
+        <Link href="/basics">
+          <p className="py-4 px-8 m-4 hover:cursor-pointer bg-slate-600 rounded-lg text-slate-100">
+            next
+          </p>
+        </Link>
+      </div>
     </div>
   );
 };
