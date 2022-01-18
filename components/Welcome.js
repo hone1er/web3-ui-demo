0825489904c3e2a1276d63485745581b8bcd3bb9
hone1er <24376928+hone1er@users.noreply.github.com>
2022-01-18 11:53:43 -0800

    added framer-motion

--- components/Welcome.js
+++ components/Welcome.js
@@ -1,12 +1,28 @@
 import React from "react";
 import DownArrow from "./DownArrow";
+import { motion } from "framer-motion";
 
 export default function Welcome() {
   return (
     <div className="flex flex-col relative w-full min-h-screen h-full justify-center items-center bg-slate-100">
-      <h1 className="text-lg sm:text-2xl md:text-4xl w-full text-center">
+      <motion.h1
+        className="text-lg sm:text-2xl md:text-4xl w-full text-center "
+        initial={{
+          scale: 0,
+        }}
+        animate={{
+          scale: 1,
+          rotateX: 360,
+          rotateZ: 360,
+        }}
+        transition={{
+          duration: 1,
+          delay: 0.1,
+          ease: "backInOut",
+        }}
+      >
         Welcome the web3-UI demo w/ NextJS!
-      </h1>
+      </motion.h1>
       <DownArrow href={"/#intro"} />
     </div>
   );
